22378fddf37ec59913a692c4e120a49136182b31
Avto <chachanidze29m@gmail.com>
2022-09-02 17:40:02 +0400

    changes

--- Test/Setup/Patch/Data/AddProduct.php
+++ Test/Setup/Patch/Data/AddProduct.php
@@ -2,41 +2,100 @@
 
 namespace Scandiweb\Test\Setup\Patch\Data;
 
+use Exception;
 use Magento\Catalog\Api\CategoryLinkManagementInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Model\Product\Visibility;
 use Magento\Framework\Exception\CouldNotSaveException;
 use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\StateException;
-use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 use Magento\Framework\App\State;
+use Magento\Store\Model\StoreManagerInterface;
 use Magento\Catalog\Api\Data\ProductInterfaceFactory;
 use Magento\Catalog\Model\Product\Type;
 use Magento\Catalog\Model\Product\Attribute\Source\Status;
 use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory as CategoryCollectionFactory;
 use Magento\Catalog\Model\Product;
 use Magento\Eav\Setup\EavSetup;
+use Magento\InventoryApi\Api\Data\SourceItemInterface;
+use Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
+use Magento\InventoryApi\Api\SourceItemsSaveInterface;
 
-
-class patch01 implements DataPatchInterface
+class AddProduct implements DataPatchInterface
 {
-    protected ModuleDataSetupInterface $setup;
+    /**
+     * @var State
+     */
     protected State $state;
+
+    /**
+     * @var ProductInterfaceFactory
+     */
     protected ProductInterfaceFactory $productFactory;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
     protected ProductRepositoryInterface $productRepository;
+
+    /**
+     * @var CategoryLinkManagementInterface
+     */
     protected CategoryLinkManagementInterface $categoryLink;
+
+    /**
+     * @var CategoryCollectionFactory
+     */
     protected CategoryCollectionFactory $categoryCollectionFactory;
+
+    /**
+     * @var EavSetup
+     */
     protected EavSetup $eavSetup;
 
+    /**
+     * @var StoreManagerInterface
+     */
+    protected StoreManagerInterface $storeManager;
+
+    /**
+     * @var SourceItemInterfaceFactory
+     */
+    protected SourceItemInterfaceFactory $sourceItemFactory;
+
+    /**
+     * @var SourceItemsSaveInterface
+     */
+    protected SourceItemsSaveInterface $sourceItemsSaveInterface;
+
+    /**
+     * @var array
+     */
+    protected array $sourceItems = [];
+
+    /**
+     * @param State $state
+     * @param ProductInterfaceFactory $productFactory
+     * @param ProductRepositoryInterface $productRepository
+     * @param CategoryLinkManagementInterface $categoryLink
+     * @param CategoryCollectionFactory $categoryCollectionFactory
+     * @param EavSetup $eavSetup
+     * @param StoreManagerInterface $storeManager
+     * @param SourceItemInterfaceFactory $sourceItemFactory
+     * @param SourceItemsSaveInterface $sourceItemsSaveInterface
+     */
     public function __construct(
-        ModuleDataSetupInterface        $setup,
         State                           $state,
         ProductInterfaceFactory         $productFactory,
         ProductRepositoryInterface      $productRepository,
         CategoryLinkManagementInterface $categoryLink,
         CategoryCollectionFactory       $categoryCollectionFactory,
-        EavSetup                        $eavSetup
+        EavSetup                        $eavSetup,
+        StoreManagerInterface           $storeManager,
+        SourceItemInterfaceFactory      $sourceItemFactory,
+        SourceItemsSaveInterface        $sourceItemsSaveInterface
     )
     {
         $this->categoryCollectionFactory = $categoryCollectionFactory;
@@ -44,25 +103,28 @@ class patch01 implements DataPatchInterface
         $this->categoryLink = $categoryLink;
         $this->productFactory = $productFactory;
         $this->productRepository = $productRepository;
-        $this->setup = $setup;
         $this->state = $state;
+        $this->storeManager = $storeManager;
+        $this->sourceItemFactory = $sourceItemFactory;
+        $this->sourceItemsSaveInterface = $sourceItemsSaveInterface;
     }
 
     /**
-     * @throws \Exception
+     * @return void
+     * @throws Exception
      */
-    public function apply()
+    public function apply(): void
     {
-        $this->setup->startSetup();
         $this->state->emulateAreaCode('adminhtml', [$this, 'execute']);
     }
 
     /**
-     * @throws StateException
      * @throws CouldNotSaveException
      * @throws InputException
+     * @throws StateException
+     * @throws LocalizedException
      */
-    public function execute()
+    public function execute(): void
     {
         $product = $this->productFactory->create();
         if ($product->getIdBySku('puma_suede')) {
@@ -70,16 +132,27 @@ class patch01 implements DataPatchInterface
         }
 
         $attributeSetId = $this->eavSetup->getAttributeSetId(Product::ENTITY, 'Default');
+        $websiteIDs = [$this->storeManager->getStore()->getWebsiteId()];
 
         $product->setTypeId(Type::TYPE_SIMPLE)
             ->setAttributeSetId($attributeSetId)
             ->setName('Puma Suede')
             ->setSku('puma_suede')
             ->setPrice(120)
+            ->setWebsiteIds($websiteIDs)
+            ->setStockData(['use_config_manage_stock' => 1, 'is_qty_decimal' => 0, 'is_in_stock' => 1])
             ->setVisibility(Visibility::VISIBILITY_BOTH)
             ->setStatus(Status::STATUS_ENABLED);
 
         $product = $this->productRepository->save($product);
+        $sourceItem = $this->sourceItemFactory->create();
+        $sourceItem->setSourceCode('default');
+        $sourceItem->setQuantity(100);
+        $sourceItem->setSku($product->getSku());
+        $sourceItem->setStatus(SourceItemInterface::STATUS_IN_STOCK);
+        $this->sourceItems[] = $sourceItem;
+
+        $this->sourceItemsSaveInterface->execute($this->sourceItems);
 
         $categoryTitles = ['Men'];
         $categoryIds = $this->categoryCollectionFactory->create()
@@ -87,15 +160,19 @@ class patch01 implements DataPatchInterface
             ->getAllIds();
 
         $this->categoryLink->assignProductToCategories($product->getSku(), $categoryIds);
-
-        $this->setup->endSetup();
     }
 
+    /**
+     * @return array
+     */
     public static function getDependencies(): array
     {
         return [];
     }
 
+    /**
+     * @return array
+     */
     public function getAliases(): array
     {
         return [];
